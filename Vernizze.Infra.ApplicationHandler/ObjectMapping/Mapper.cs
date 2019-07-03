04293d5a0e0829e9d2f95efde090e0e6f94cc02e
Carlos Vernizze <carlos.vernizze@outlook.com>
2019-07-03 13:40:30 -0300

    Implementacao do mapeamento de objetos (via Mapster)

---
+++ Vernizze.Infra.ApplicationHandler/ObjectMapping/Mapper.cs
@@ -0,0 +1,69 @@
+﻿using Mapster;
+using Newtonsoft.Json;
+using Vernizze.Infra.ApplicationHandler.VOs;
+using Vernizze.Infra.CrossCutting.Utils;
+using Vernizze.Infra.CrossCutting.Extensions;
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Vernizze.Infra.ApplicationHandler.ObjectMapping
+{
+    public class Mapper
+    {
+        static Mapper()
+        {
+            new Mapper().Init();
+        }
+
+        private void Init()
+        {
+            var mapping_configs = ReadConfigs();
+
+            if (mapping_configs.IsNotNull() && mapping_configs.configs.HaveAny())
+            {
+                mapping_configs.configs.ForEach(i =>
+                {
+                    string typeName01 = string.Concat(Assembly.GetExecutingAssembly().GetName().Name, ".", i.object_name_origin);
+                    Type MyType01 = Type.GetType(typeName01);
+
+                    string typeName02 = string.Concat(Assembly.GetExecutingAssembly().GetName().Name, ".", i.object_name_destiny);
+                    Type MyType02 = Type.GetType(typeName02);
+
+                    var typeOfContext = this.GetType();
+
+                    var method = typeOfContext.GetMethod("Config");
+
+                    var genericMethod = method.MakeGenericMethod(MyType01, MyType02);
+
+                    genericMethod.Invoke(this, new object[] { i.relations.ToArray() });
+                });
+            }
+        }
+
+        public void Config<TOrigin, TDestiny>(object[] parameters)
+        {
+            var relations = parameters.ToList();
+
+            var adapter = TypeAdapterConfig<TOrigin, TDestiny>.NewConfig();
+
+            if (relations.HaveAny())
+                relations.ForEach(r =>
+                {
+                    adapter.Map((r as ObjectMappingRelationsConfigs).atribute_destiny, (r as ObjectMappingRelationsConfigs).atribute_origin);
+                });
+        }
+
+        public static TDestiny Map<TOrigin, TDestiny>(TOrigin origin)
+        {
+            return origin.Adapt<TDestiny>();
+        }
+
+        private static ObjectMappingConfigs ReadConfigs()
+        {
+            var configs_string_content = FilesAndFolders.GetFileContent("object_mapping.json");
+
+            return JsonConvert.DeserializeObject<ObjectMappingConfigs>(configs_string_content); ;
+        }
+    }
+}
---
+++ Vernizze.Infra.ApplicationHandler/VOs/ObjectMappingConfigs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Vernizze.Infra.ApplicationHandler.VOs
+{
+    internal class ObjectMappingConfigs
+    {
+        public List<ObjectMappingConfig> configs { get; set; }
+    }
+
+    internal class ObjectMappingConfig
+    {
+        public string object_name_origin { get; set; }
+
+        public string object_name_destiny { get; set; }
+
+        public List<ObjectMappingRelationsConfigs> relations { get; set; }
+    }
+
+    internal class ObjectMappingRelationsConfigs
+    {
+        public string atribute_origin { get; set; }
+        public string atribute_destiny { get; set; }
+    }
+}
